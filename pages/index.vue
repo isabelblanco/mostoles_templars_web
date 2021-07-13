9b85601dd7573d4a9fb26af2744b2f7c8e3cb2f8
isabelblanco <isabelblancodura@gmail.com>
2021-07-13 17:44:41 +0200

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <v-row>
+    <v-col cols="12" align="center">
+      <p class="title">
+        In construction
+      </p>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" align="center">
+      <img src="~/assets/images/solo_templario.png" alt="caballero templario">
+    </v-col>
+  </v-row>
+</div>
+</template>
+
+<style scoped>
+  .title {
+    font-size: 10em;
+    color: #08a0c7;
+  }
+
+  img {
+    width: 750px;
+  }
+</style>
